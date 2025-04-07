3a20ecaf085b03b09160a0b885d10861327bd350
Hanka <hrosicka@post.cz>
2025-04-07 18:22:34 +0200

    Upload - refactoring

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import io
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -8,17 +9,20 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         if 'file' not in request.files:
-            return 'No file selected'
+            return 'Nebyl vybrán žádný soubor.'
         file = request.files['file']
         if file.filename == '':
-            return 'No file selected'
+            return 'Nebyl vybrán žádný soubor.'
         if file:
+            filename = secure_filename(file.filename) #Bezpečnost
+            if not filename.endswith(('.xls', '.xlsx')):
+                return 'Neplatný formát souboru. Nahrajte prosím Excel soubor.'
             try:
                 df = pd.read_excel(io.BytesIO(file.read()))
                 table_html = df.to_html(classes='data-table')
                 return render_template('index.html', table_html=table_html)
             except Exception as e:
-                return f'Error processing file: {e}'
+                return f'Chyba při zpracování souboru: {e}'
     return render_template('index.html')
 
 if __name__ == '__main__':
